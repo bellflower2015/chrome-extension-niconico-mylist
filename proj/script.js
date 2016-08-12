13a87df3bf202393efcdc33b990982b1efd811de
Kikyou Akino <bellflower@web4u.jp>
2016-08-12 17:47:34 +0900

    Add support for public mylist
    (e.g. http://www.nicovideo.jp/mylist/NNNNNNNN)

--- proj/script.js
+++ proj/script.js
@@ -1,4 +1,5 @@
 var pollWrapper;
+var type; // 'mypage', 'public'
 
 var mylistitem = [];
 
@@ -47,62 +48,92 @@ var select2FormatResult = function(state) {
 };
 
 var display = function(href, hash) {
-    if (hash.match(/mymemory/)) {
-        $('div#mySearch').hide();
-    } else if (!href.match(/www.nicovideo.jp\/my\/mylist/)) {
-        $('div#mySearch').hide();
-    } else {
+    if (!href.match(/www.nicovideo.jp\/my\/mylist/)) {
+        if (hash.match(/mymemory/)) {
+            $('div#mySearch').hide();
+        } else {
+            $('div#mySearch').show();
+        }
+    } else if (!href.match(/www.nicovideo.jp\/mylist/)) {
         $('div#mySearch').show();
+    } else {
+        $('div#mySearch').hide();
+    }
+};
+
+var prepareData = function(data) {
+    mylistitem = [];
+    $('select#mySearchSelect option').remove();
+    $('select#mySearchSelect').append($('<option>'));
+    if (data && data.mylistitem) {
+        data.mylistitem.sort(function(a,b) {
+            if (a.create_time > b.create_time) return -1;
+            if (a.create_time < b.create_time) return 1;
+            return 0;
+        });
+        $.each(data.mylistitem, function(i, elem) {
+            if (elem.item_data.video_id) {
+                $('select#mySearchSelect').append($('<option>').text(elem.item_data.title).attr('value', elem.item_data.video_id));
+                mylistitem[elem.item_data.video_id] = {
+                    watch_id:       elem.item_data.watch_id,
+                    title:          elem.item_data.title,
+                    thumbnail_url:  elem.item_data.thumbnail_url,
+                    length_seconds: elem.item_data.length_seconds,
+                    view_counter:   elem.item_data.view_counter,
+                    num_res:        elem.item_data.num_res,
+                    mylist_counter: elem.item_data.mylist_counter,
+                    first_retrieve: elem.item_data.first_retrieve,
+                    create_time:    elem.create_time
+                };
+            }
+        });
     }
 };
 
 var getMyList = function(href, hash) {
-    var url = 'http://www.nicovideo.jp/api/deflist/list';
+    if (type == 'mypage') {
+        var url = 'http://www.nicovideo.jp/api/deflist/list';
 
-    if (hash.match(/\d+/)) {
-        var num = hash.match(/\d+/);
-        url = 'http://www.nicovideo.jp/api/mylist/list?group_id=' + num;
-    }
+        if (hash.match(/\d+/)) {
+            var num = hash.match(/\d+/);
+            url = 'http://www.nicovideo.jp/api/mylist/list?group_id=' + num;
+        }
 
-    mylistitem = [];
-    $('select#mySearchSelect option').remove();
-    $('select#mySearchSelect').append($('<option>'));
-    $.ajax({
-        type: 'GET',
-        url: url,
-        dataType: 'json',
-        success: function(data) {
-            if (data && data.mylistitem) {
-                data.mylistitem.sort(function(a,b) {
-                    if (a.create_time > b.create_time) return -1;
-                    if (a.create_time < b.create_time) return 1;
-                    return 0;
-                });
-                $.each(data.mylistitem, function(i, elem) {
-                    if (elem.item_data.video_id) {
-                        $('select#mySearchSelect').append($('<option>').text(elem.item_data.title).attr('value', elem.item_data.video_id));
-                        mylistitem[elem.item_data.video_id] = {
-                            watch_id:       elem.item_data.watch_id,
-                            title:          elem.item_data.title,
-                            thumbnail_url:  elem.item_data.thumbnail_url,
-                            length_seconds: elem.item_data.length_seconds,
-                            view_counter:   elem.item_data.view_counter,
-                            num_res:        elem.item_data.num_res,
-                            mylist_counter: elem.item_data.mylist_counter,
-                            first_retrieve: elem.item_data.first_retrieve,
-                            create_time:    elem.create_time
-                        };
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            success: function(data) {
+                prepareData(data);
+            }
+        });
+        display(href, hash);
+    } else if (type == 'public') {
+        if ($('.SYS_box_filter strong') && $('.SYS_box_filter strong').text().length > 0) {
+            var script = $('script').text();
+            if (script && script.length > 0) {
+                var match;
+                if (match = script.match(/Mylist\.preload\(\d+, (.*)\);/)) {
+                    if (match && match[1] && match[1].length > 0) {
+                        var data = {mylistitem: []};
+                        eval('data.mylistitem = ' + match[1]);
+                        prepareData(data);
                     }
-                });
-
+                }
             }
         }
-    });
-    display(href, hash);
+        display(href, hash);
+    }
 };
 
 var createSelectTag = function() {
-    $('div.wrapper').before('<div class="my-search" id="mySearch"><h3>マイリスト検索</h3><div class="my-search-box"><select id="mySearchSelect"></select></div></div>');
+    if (type == 'mypage') {
+        $('div.wrapper').before('<div class="my-search" id="mySearch"><h3>マイリスト検索</h3><div class="my-search-box"><select id="mySearchSelect"></select></div></div>');
+    } else if (type == 'public') {
+        $('ul.tabMenu').before('<div class="my-search" id="mySearch" style="width: 98%;"><h3>マイリスト検索</h3><div class="my-search-box" style="width: 630.55px;"><select id="mySearchSelect"></select></div></div>');
+    } else {
+        return;
+    }
 
     getMyList(window.location.href, window.location.hash);
 
@@ -141,6 +172,11 @@ var createSelectTag = function() {
 var waitWrapper = function() {
     if ($('div.wrapper') && $('div.wrapper')[0] && pollWrapper) {
         clearInterval(pollWrapper);
+        type = 'mypage';
+        createSelectTag();
+    } else if ($('div#SYS_page_items') && $('div#SYS_page_items')[0] && pollWrapper) {
+        clearInterval(pollWrapper);
+        type = 'public';
         createSelectTag();
     }
 };
@@ -148,6 +184,8 @@ var waitWrapper = function() {
 
 $(function() {
     if (document.getElementById("mylist") != null) {
+        pollWrapper = setInterval(waitWrapper, 100);
+    } else if (document.getElementById("SYS_box_mylist_body") != null) {
         pollWrapper = setInterval(waitWrapper, 100);
     }
 });
